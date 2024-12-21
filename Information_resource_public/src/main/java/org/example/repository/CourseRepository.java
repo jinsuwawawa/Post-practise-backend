05524406c96d03208ffe5c14bf947f729d67f817
szuhe <2216080165@qq.com>
2024-12-21 19:12:25 +0800

    Delete Information_resource_public directory

--- Information_resource_public/src/main/java/org/example/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
--- Information_resource_public/src/main/java/org/example/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
